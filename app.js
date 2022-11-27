81898eec810352629c89fe0f38c058c7af701ea1
Musab Hassan <musabhassan1999@gmail.com>
2022-11-27 20:52:22 +0530

    update

--- app.js
+++ app.js
@@ -3,30 +3,41 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food","Cook Food","Eat Food"];
+let items = ["Buy Food","Cook Food","Eat Food"];
+let works = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    var today = new Date();
-    var options = {
+    let today = new Date();
+    let options = {
         weekday : "long",
         day : "numeric",
         month : "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {kindOfDay : day, newListItems : items});
+    res.render("list", {ListTitle : day, newListItems : items});
 });
 
 app.post("/", function(req, res) {
-    var item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    let item = req.body.newItem;
+    if(req.body.button === "Work"){
+        works.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+   
 } );
+
+app.get("/work", function(req, res) {
+    res.render("list",{ListTitle : "Work List", newListItems : works});
+})
 
 app.listen(3000, function(){
     console.log("server started");
