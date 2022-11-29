f37eee6facc80e1c8403b6845d4055d1c00b0118
Musab Hassan <musabhassan1999@gmail.com>
2022-11-29 12:15:19 +0530

    final commit - v1

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food","Cook Food","Eat Food"];
-let works = [];
+const items = ["Buy Food","Cook Food","Eat Food"];
+const works = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    let today = new Date();
-    let options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
+    let day = date.getDate();
     res.render("list", {ListTitle : day, newListItems : items});
 });
 
